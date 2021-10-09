8498a5a7a364f6cee6263242990cadfc7a711324
Kinley Davis <d8tascientist@gmail.com>
2021-10-09 11:14:07 -0400

    coding new page to register new user

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,14 @@
+// Reference 22-State > 23-Ins-Stripe
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+            }
+        }
+    }
+`;
+
