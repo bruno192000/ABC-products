3622b37fecdde7f4ad77bb0314213e645d0e2fe1
Kinley Davis <d8tascientist@gmail.com>
2021-10-09 11:41:22 -0400

    updating code for Signup, created Login page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -2,5 +2,49 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
-import Auth from '../utils/mutations';
+import Auth, { ADD_USER } from '../utils/mutations';
 
+function Signup(props) {
+    const [formState, setFormState] = useState({ email: '', password: ''});
+    const [addUser] = useMutation(ADD_USER);
+
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        const mutationResponse = await addUser({
+            variables: {
+                email: formState.email,
+                password: formState.password,
+                firstName: formState.firstName,
+                lastName: formState.lastName,
+            },
+        });
+
+        const token = mutationResponse.data.addUser.token;
+        Auth.login(token);
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    // Dynamic Signup Webpage
+    return(
+        <div className="container my-1">
+            <Link to="/login">← Go to Login</Link>
+
+            <h2>Signup</h2>
+            <form onSubmit={handleFormSubmit}>
+                <div className="flex-row space-between my-2">
+                    
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Signup;
