b08fabbdf9d89858186365484c4b16d1632d2b24
Kinley Davis <d8tascientist@gmail.com>
2021-10-11 20:58:47 -0400

    corrected code

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ import { Provider } from 'react-redux';
 import store from './utils/store';
 
 import Home from "./pages/Home";
-import Detail from "./pages/Detail.js";
+import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
